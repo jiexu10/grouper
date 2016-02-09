5c43d4b72e1571dcfa95e5e6a2d419b3f975ae8c
jiexu10 <jie.xu.10@gmail.com>
2016-02-09 18:54:23 -0500

    working index and create

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      jsonapi_resources :players, only: [:index, :show]
+      jsonapi_resources :players, only: [:index, :show, :create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
